600be39baf303091ae9d9895cbe20eefed8e9aff
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-02-17 22:09:37 +0530

    Update Navbar.jsx

--- Frontend/src/components/Layout/Navbar.jsx
+++ Frontend/src/components/Layout/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     try {
-      const response = await axios.get("http://localhost:4000/api/v1/user/logout", {
+      const response = await axios.get("https://vercel.com/prateeks-projects-ce3bdc61/job-seeker-server/Ae3Px9XFBXdYnJzSsDWwE7RokSJQ/api/v1/user/logout", {
         withCredentials: true,
       });
       toast.success(response.data.message);
