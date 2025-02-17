a272d145048bb3c05e732a1e64ac3a85df922c9a
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-02-17 22:13:44 +0530

    Update Jobs.jsx

--- Frontend/src/components/Job/Jobs.jsx
+++ Frontend/src/components/Job/Jobs.jsx
@@ -12,7 +12,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("https://vercel.com/prateeks-projects-ce3bdc61/job-seeker-server/Ae3Px9XFBXdYnJzSsDWwE7RokSJQ/api/v1/job/getall", {
+        .get("https://job-seeker-server-tau.vercel.app/api/v1/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
